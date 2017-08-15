7145ec84e0d53ff4d4f91f2f8d219c5cc140a324
孤鸿 夜 <litiansum@foxmail.com>
2017-08-15 15:52:59 +0800

    升级日志组件

--- Magicodes.Sms/pack.Magicodes.Sms.Aliyun.bat
+++ Magicodes.Sms/pack.Magicodes.Sms.Aliyun.bat
@@ -1,2 +1,2 @@
-call ./pack.bat "Magicodes.Sms.Aliyun.nupkg" "Magicodes.Sms.Aliyun/Magicodes.Sms.Aliyun.csproj"
+call ./pack.bat "Magicodes.Sms.Aliyun.2.0.0.nupkg" "Magicodes.Sms.Aliyun/Magicodes.Sms.Aliyun.csproj"
 @pause
